6feff523081d13296f30471314aa584eacb5bac6
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2020-10-13 18:01:45 -0300

    setup axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './quasar'
+import axios from 'axios'
+
+window.axios = axios
+axios.defaults.baseURL = 'http://127.0.0.1:9090'
 
 Vue.config.productionTip = false
 
